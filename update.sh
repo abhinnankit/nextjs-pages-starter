c50bf908e47b9f5d852f790c9cdf413bb23d3599
Abhinn Ankit <ankit.a@husky.neu.edu>
2023-09-30 14:22:59 -0400

    Add no husky precommit support

--- update.sh
+++ update.sh
@@ -13,6 +13,21 @@ fi
 echo -e "Adding files:\n"
 git add --all
 
+PRECOMMMIT_FILE=.husky/pre-commit
+
+if [ ! -f "$PRECOMMMIT_FILE" ]; then
+    echo -e "Please configure husky precommit hook"
+    echo -e "Formatting only staged files\n"
+    nr precommit
+
+    echo -e "Checking lint errors on staged files\n"
+    nr lint --cache
+
+    if [ $? -ne 0 ]; then
+        echo -e "Lint error\n"
+        exit 1
+    fi
+fi
 
 echo -e "Committing changes.\n"
 git commit -S -m "${commit_msg}"
