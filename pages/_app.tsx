4e4135abde8ca6b748d64115f543830823d96cd4
Abhinn Ankit <ankit.a@husky.neu.edu>
2023-09-30 04:16:12 -0400

    Add global font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
+import { catamaran } from '@/common/utils/fonts';
 import '@/styles/globals.scss';
 
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${catamaran.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
